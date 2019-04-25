1a543979947ecb5c9c7916f627c9e56e297fdeab
MAURO ALEJANDRO HERNANDEZ DE LAS MUÑECAS <mauroahdlm@gmail.com>
2019-04-25 16:33:48 -0400

    commit proyecto mvc

---
+++ TAREA1/MVCTAREA1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCTAREA1.MvcApplication" Language="C#" %>
---
+++ TAREA1/MVCTAREA1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCTAREA1.Startup))]
+namespace MVCTAREA1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
