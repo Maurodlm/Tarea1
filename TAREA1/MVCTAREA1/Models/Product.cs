3c6ff93b0b4b621092497d3325de08db33c7a2ce
MAURO ALEJANDRO HERNANDEZ DE LAS MUÑECAS <mauroahdlm@gmail.com>
2019-04-25 17:19:44 -0400

    commit modelo mvc

---
+++ TAREA1/MVCTAREA1/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCTAREA1.Models
+{
+    public class Product
+    {
+
+        [Key]
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ TAREA1/MVCTAREA1/Models/Sale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCTAREA1.Models
+{
+    public class Sale
+    {
+        [Key]
+        public int SaleId { get; set; }
+        public int ProductId { get; set; }
+        public int ClientId { get; set; }
+        public int Date { get; set; }
+    }
+}
